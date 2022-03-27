ff6ffa09c2b6c9f32ac4db44084a472e4ecbf93c
Bilal Raza <smbraza97@gmail.com>
2022-03-27 18:29:48 +0500

    finished adding resolvers, login and addUser were recycled from instructor files

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,4 +2,76 @@ const { AuthenticationError } = require("apollo-server-express");
 const { User } = require("../models");
 const { signToken } = require("../utils/auth");
 
+const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id }).select(
+          "-__v -password"
+        );
+        return userData;
+      }
+      throw new AuthenticationError("You are not logged in!");
+    },
+  },
+
+  Mutation: {
+    addUser: async (parent, { username, email, password }) => {
+      // First we create the user
+      const user = await User.create({ username, email, password });
+      // To reduce friction for the user, we immediately sign a JSON Web Token and log the user in after they are created
+      const token = signToken(user);
+      // Return an `Auth` object that consists of the signed token and user's information
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      // Look up the user by the provided email address. Since the `email` field is unique, we know that only one person will exist with that email
+      const user = await User.findOne({ email });
+
+      // If there is no user with that email address, return an Authentication error stating so
+      if (!user) {
+        throw new AuthenticationError("No user found with this email address!");
+      }
+
+      // If there is a user found, execute the `isCorrectPassword` instance method and check if the correct password was provided
+      const correctPw = await user.isCorrectPassword(password);
+
+      // If the password is incorrect, return an Authentication error stating so
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect credentials.");
+      }
+
+      // If email and password are correct, sign user into the application with a JWT
+      const token = signToken(user);
+
+      // Return an `Auth` object that consists of the signed token and user's information
+      return { token, user };
+    },
+    saveBook: async (parent, { input }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          // We are adding this new book to the array of saved books
+          { $addToSet: { savedBooks: input } },
+          { new: true, runValidators: true }
+        );
+        return updatedUser;
+      }
+      throw new AuthenticationError("You need to be logged in to save a book!");
+    },
+    removeBook: async (parent, { bookId }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          // We are deleting this book from the savedBooks array
+          { $pull: { savedBooks: { bookId: bookId } } },
+          { new: true }
+        );
+        return updatedUser;
+      }
+      throw new AuthenticationError("You need to be logged in to delete a book!");
+    },
+  },
+};
+
 module.exports = resolvers;
